68e4cd69c34790007510a77faa0639a4ff630c9f
Purusoth <purusoth.shanmugaragah@live.vu.edu.au>
2020-12-17 11:38:31 +0530

    third

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'maps',
     'herokuapp',
 ]
 
@@ -77,7 +78,9 @@ WSGI_APPLICATION = 'djangoherokuapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 
